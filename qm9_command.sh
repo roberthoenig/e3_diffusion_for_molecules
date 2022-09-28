91436159e3b361949eac9323e4070aeaa8f6aa34
Robert Hönig <robhoenig@gmail.com>
2022-09-28 17:04:06 +0200

    Add original training command.

---
+++ qm9_command.sh
@@ -0,0 +1,1 @@
+python main_qm9.py --n_epochs 3000 --exp_name edm_qm9 --n_stability_samples 1000 --diffusion_noise_schedule polynomial_2 --diffusion_noise_precision 1e-5 --diffusion_steps 1000 --diffusion_loss_type l2 --batch_size 64 --nf 256 --n_layers 9 --lr 1e-4 --normalize_factors [1,4,10] --test_epochs 20 --ema_decay 0.9999
